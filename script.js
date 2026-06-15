07141d51cd338b9a2ffac7b98e14023be25a58a0
PuneetVerma07 <pk2382849@gmail.com>
2026-06-15 10:41:50 +0530

    todo program by using function array and object

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-let num1 = Number(prompt("Enter first number"))
-let operator = prompt("Enter operation ( + - * / ) : ")
-let num2 = Number(prompt("Enter second number"))
+let todos = []
 
-let result;
+function addTodo(task) {
+    todos.push({id: todos.length + 1, task, done: false})
+}
 
-if (operator == "+") result = num1 + num2;
-else if (operator == "-") result = num1 - num2
-else if (operator == "*") result = num1 * num2;
-else if (operator == "/") result = num2 !== 0 ? num1/num2 : "Can not divided by zero"
-else "Invalid operator"
+function completeTodo(id) {
+    let todo = todos.find(t => t.id == id)
+    if(todo) todo.done = true
+}
 
-console.log(result)
+function removeTodo(id) {
+    todos = todos.filter(t => t.id !==  id)
+}
+
+function showTodos() {
+    todos.forEach((t) => {
+        console.log(`${t.id}. [${t.done ? "x" : " "}] ${t.task}`)
+    })
+}
+
+addTodo("Learn JS")
+addTodo("Build project")
+addTodo("sleep")
+completeTodo(1)
+removeTodo(3)
+showTodos()
