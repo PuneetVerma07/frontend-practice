889f8f74e586c20f437ecbcf537c002115927cc5
PuneetVerma07 <pk2382849@gmail.com>
2026-06-17 12:48:19 +0530

    library management system done

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-/* word frequency counter */
+let library = {
+    books: [],
 
-function wordFrequency(text) {
-    let words = text.toLowerCase().split(/\s+/);
-    let freq = {}
+    addBook(title, author) {
+        this.books.push({
+            id: this.books.length + 1,
+            title,
+            author,
+            borrowed: false
+        });
+    },
 
-    words.forEach((word) => {
-        freq[word] = (freq[word] || 0) + 1;
-    })
+    borrowBook(id) {
+        let book = this.books.find(b => b.id === id);
+        if (!book) return "Book not found";
+        if (book.borrowed) return "Already borrowed";
+        book.borrowed = true;
+        return `Your borrowed "${book.title}"`
+    },
 
-    return freq;
+    returnBook(id) {
+        let book = this.books.find(b => b.id === id);
+        if (!book) return "Book not found";
+        book.borrowed = false;
+        return `You returned "${book.title}"`
+    },
+
+    availableBooks() {
+        return this.books.filter(b => !b.borrowed )
+    }
 }
 
-let text = "the quick brown fox jumps over the lazy dog the fox is quick"
-
-console.log(wordFrequency(text))
-
+library.addBook("atomic habits", "james cleaner")
+library.addBook("deep work", "cal newport")
+library.addBook("devine work", "newport")
+console.log(library.borrowBook(1))
+console.log(library.returnBook(2))
+console.log(library.availableBooks())
