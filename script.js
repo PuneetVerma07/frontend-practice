dcc9da45fed5418037e7b25150ffb5d55fa800e6
PuneetVerma07 <pk2382849@gmail.com>
2026-06-18 10:23:25 +0530

    closures practice done

--- script.js
+++ script.js
@@ -1,38 +1,14 @@
-let library = {
-    books: [],
+function createBankAccount() {
+    let balance = 0;
 
-    addBook(title, author) {
-        this.books.push({
-            id: this.books.length + 1,
-            title,
-            author,
-            borrowed: false
-        });
-    },
-
-    borrowBook(id) {
-        let book = this.books.find(b => b.id === id);
-        if (!book) return "Book not found";
-        if (book.borrowed) return "Already borrowed";
-        book.borrowed = true;
-        return `Your borrowed "${book.title}"`
-    },
-
-    returnBook(id) {
-        let book = this.books.find(b => b.id === id);
-        if (!book) return "Book not found";
-        book.borrowed = false;
-        return `You returned "${book.title}"`
-    },
-
-    availableBooks() {
-        return this.books.filter(b => !b.borrowed )
-    }
+    return {
+        deposit(amount) { balance += amount; return balance; },
+        getBalance() { return balance; }
+    };
 }
 
-library.addBook("atomic habits", "james cleaner")
-library.addBook("deep work", "cal newport")
-library.addBook("devine work", "newport")
-console.log(library.borrowBook(1))
-console.log(library.returnBook(2))
-console.log(library.availableBooks())
+const acc = createBankAccount()
+
+acc.deposit(100)
+
+console.log(acc.getBalance())
