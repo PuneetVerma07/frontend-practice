39afca80af19db63f15f0037724b999a511aff00
PuneetVerma07 <pk2382849@gmail.com>
2026-06-18 17:41:59 +0530

    constructor function practice done

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-class MakeStudents {
-    constructor(fname, lname, contact, isVerified) {
-        this.fname = fname;
-        this.lname = lname;
-        this.contact = contact;
-        this.isVerified = isVerified;
-    }
+function MakeStudents(fname, lname, contact, isVerified) {
+    this.fname = fname;
+    this.lname = lname;
+    this.contact = contact;
+    this.isVerified = isVerified;
+}
 
-    showProfile = function () {
-        if (this.isVerified) {
-            console.log(`${this.fname} ${this.lname}`)
-        } else {
-            console.log("User not verified.")
-        }
+MakeStudents.prototype.showProfile = function () {
+    if (this.isVerified) {
+        console.log(`${this.fname} ${this.lname}`)
+    } else {
+        console.log("User not found")
     }
 }
 
-let s1 = new MakeStudents("aman","verma", 999, true)
+let s1 = new MakeStudents("puneet", "verma", 999, true)
+
 console.log(s1)
+
+s1.showProfile();
+
