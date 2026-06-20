4c6dd00f36ca9772fa4af78b1c8ab2cf062947ca
PuneetVerma07 <pk2382849@gmail.com>
2026-06-21 03:47:55 +0530

    understanding of setTimeout and practice

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-function greet(name) {
-    console.log("Hello " + name)
-}
+let arr = [ 1,2,3]
 
-function welcome(callback) {
-    let user = "Rahul";
-    callback(user)
-}
+arr.forEach(function (num) {
+    console.log(num * 2)
+})
 
-welcome(greet);
+setTimeout(function(){console.log("excutes later")}, 3000)
+
+setTimeout(function(){console.log("2 seconds later")}, 2000)
+
+setTimeout(function () {
+    console.log("Hello")
+}, 0)
+
+console.log("world")
