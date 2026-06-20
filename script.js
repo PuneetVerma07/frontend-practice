7d1a6d34581464bcd772f4796ef551ed6aeb452d
PuneetVerma07 <pk2382849@gmail.com>
2026-06-21 05:26:59 +0530

    setTimeout and setInterval() practices with clearInterval()

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-let arr = [ 1,2,3]
+let count = 1;
 
-arr.forEach(function (num) {
-    console.log(num * 2)
-})
-
-setTimeout(function(){console.log("excutes later")}, 3000)
-
-setTimeout(function(){console.log("2 seconds later")}, 2000)
+let timer = setInterval(function () {
+    console.log(count)
+    count++;
+}, 1000)
 
 setTimeout(function () {
-    console.log("Hello")
-}, 0)
+    clearInterval(timer)
+}, 5000);
 
-console.log("world")
+
